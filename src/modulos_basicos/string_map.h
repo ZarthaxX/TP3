da40afca5dd8aac4e15040d55aaf443c610690c4
Unknown <ogianatiempo@gmail.com>
2019-06-28 19:25:43 -0300

    implementacion string_map y test fallido

---
+++ src/modulos_basicos/string_map.h
@@ -0,0 +1,165 @@
+#ifndef STRING_MAP_H_
+#define STRING_MAP_H_
+
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+namespace algo2 { 
+
+template<typename T>
+class string_map {
+public:
+    /**
+    CONSTRUCTOR
+    * Construye un diccionario vacio.
+    **/
+    string_map();
+
+    /**
+    CONSTRUCTOR POR COPIA
+    * Construye un diccionario por copia.
+    **/
+    string_map(const string_map<T>& aCopiar);
+
+    /**
+    OPERADOR ASIGNACION
+     */
+    string_map& operator=(const string_map& d);
+
+    /**
+    DESTRUCTOR
+    **/
+    ~string_map();
+
+    /**
+    DEFINIR
+     * Define un nuevo par clave/valor
+     */
+
+    void Definir(const string &clave, T valor);
+
+    /**
+    DEFINIDO
+     * Devuelve True si y solo si la clave esta definida
+     */
+
+    bool Definido(const string &clave) const;
+
+    /**
+    SIGNIFICADO
+     * Devuelve una referencia inmutable o mutable al significado.
+     * PRE: La clave está definida.
+     */
+
+    const T& Significado(const string &clave) const;
+    T& Significado(const string &clave);
+
+    /**
+    * KEYS
+    * Devuelve el conjunto de claves por referencia inmutable.
+    **/
+
+    const set<string>& Claves() const;
+
+    /**
+    ERASE
+    * Dada una clave, la borra del diccionario junto a su significado.
+    * PRE: La clave está definida.
+    **/
+    void Borrar(const string& clave);
+
+private:
+
+    struct Nodo {
+        vector<Nodo*> siguientes;
+        T* definicion;
+        set<string>::iterator itClave;
+
+        Nodo(set<string>& c) : siguientes(256, NULL), definicion(NULL), itClave(c.begin()) {};
+//        Nodo(T* def) : siguientes(256, NULL), definicion(def) {};
+    };
+
+    Nodo* _raiz;
+    set<string> _claves;
+
+    Nodo* copiadoRecursivo(Nodo* n) {
+        // COPIAR CLAVES AL CONJUNTO
+
+        // Si n existe
+        if (n != NULL){
+            // Creo un nuevo nodo
+            Nodo* nuevo = new Nodo(_claves);
+            // Copio la definicion si tiene, agrego la clave al conjunto de claves
+            // y apunto el iterador del nodo a la clave
+            if (n->definicion != NULL){
+                nuevo->definicion = new T(*(n->definicion));
+                nuevo->itClave = _claves.insert(n->itClave);
+            }
+            //Copio recursivamente los siguientes
+            for (int i = 0; i < 256; ++i) {
+                if (n->siguientes[i] != NULL){
+                    nuevo->siguientes[i] = copiadoRecursivo(n->siguientes[i]);
+                }
+            }
+            // Retorno
+            return nuevo;
+        } else {
+            return NULL;
+        }
+    };
+
+    Nodo* obtenerOCrearSiguiente(Nodo* n, char c) {
+        if (n->siguientes[int(c)] == NULL) {
+            n->siguientes[int(c)] = new Nodo(_claves);
+        }
+        return n->siguientes[int(c)];
+    };
+
+    void borradoRecursivo(Nodo* n) {
+        if(n) {
+            // Borro la definicion y su clave del conjunto _claves
+            borradoRecursivoSiguientes(n->siguientes);
+            delete n->definicion;
+            _claves.erase(n->itClave);
+            delete n;
+        }
+    };
+
+    void borradoRecursivoSiguientes(vector<Nodo*> s) {
+        for (Nodo* n : s){
+            borradoRecursivo(n);
+        }
+    };
+
+    bool nodoRelevante(const Nodo* n) const {
+        unsigned int hijos = 0;
+        if (n->definicion){
+            return true;
+        }
+        for (int i = 0; i < 256; ++i) {
+            if (n->siguientes[i] != NULL) {
+                hijos++;
+            }
+        }
+        return hijos > 1;
+    };
+
+    bool tieneHijos(const Nodo* n) const {
+        unsigned int hijos = 0;
+        for (int i = 0; i < 256; ++i) {
+            if (n->siguientes[i] != NULL) {
+                hijos++;
+            }
+        }
+        return hijos > 0;
+    }
+};
+
+}
+
+#include "string_map.hpp"
+
+#endif // STRING_MAP_H_
---
+++ src/modulos_basicos/string_map.hpp
@@ -0,0 +1,124 @@
+#include "string_map.h"
+
+using namespace algo2;
+
+template <typename T>
+string_map<T>::string_map(){
+    _raiz = NULL;
+}
+
+template <typename T>
+string_map<T>::string_map(const string_map<T>& aCopiar) : string_map() { *this = aCopiar; } // Provisto por la catedra: utiliza el operador asignacion para realizar la copia.
+
+template <typename T>
+string_map<T>& string_map<T>::operator=(const string_map<T>& d) {
+    borradoRecursivo(_raiz);
+    _raiz = copiadoRecursivo(d._raiz);
+}
+
+template <typename T>
+string_map<T>::~string_map(){
+    borradoRecursivo(_raiz);
+}
+
+template <typename T>
+void string_map<T>::Definir(const string &clave, T valor) {
+    // Si la raiz no existe la creo
+    if (_raiz == NULL) {
+        _raiz = new Nodo(_claves);
+    }
+
+    // Recorro
+    Nodo* actual = _raiz;
+    for (char c : clave) {
+        actual = obtenerOCrearSiguiente(actual, c);
+    }
+
+    // Si no tiene definicion la creo, agrego la clave al conjunto de claves
+    // y apunto el iterador del nodo a la clave
+    if (actual->definicion == NULL) {
+        actual->definicion = new T(valor);
+        actual->itClave = _claves.insert(clave).first;
+    } else {
+        delete actual->definicion;
+        actual->definicion = new T(valor);
+    }
+};
+
+template <typename T>
+bool string_map<T>::Definido(const string &clave) const{
+    Nodo* actual = _raiz;
+    int i = 0;
+    while (i < clave.size() and actual->siguientes[int(clave[i])] != NULL) {
+        actual = actual->siguientes[int(clave[i])];
+        i++;
+    }
+    if (i == clave.size() and actual->significado != NULL){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+template <typename T>
+const T& string_map<T>::Significado(const string &clave) const{
+    Nodo* actual = _raiz;
+    for (char c : clave) {
+        actual = actual->siguientes[int(c)];
+    }
+    return *(actual->definicion);
+};
+
+template <typename T>
+T& string_map<T>::Significado(const string &clave) {
+    Nodo* actual = _raiz;
+    for (char c : clave) {
+        actual = actual->siguientes[int(c)];
+    }
+    return *(actual->definicion);
+};
+
+template <typename T>
+const set<string>& string_map<T>::Claves() const {
+    return _claves;
+};
+
+template <typename T>
+void string_map<T>::Borrar(const string& clave) {
+    Nodo* actual = _raiz;
+    Nodo* ultimoRelevante = NULL;
+    int indiceArreglar;
+    Nodo* borrarDesde = _raiz;
+
+    // Busco la clave
+    for (char c : clave) {
+        if (nodoRelevante(actual)) {
+            // Si el nodo es relevante, lo guardo y tengo que borrar desde el siguiente a el.
+            // Tambien guardo indice del puntero que voy a tener que arreglar dsp de borrar
+            ultimoRelevante = actual;
+            indiceArreglar = int(c);
+            actual = actual->siguientes[int(c)];
+            borrarDesde = actual;
+        } else {
+            // Si no es relevante, sigo avanzando.
+            actual = actual->siguientes[int(c)];
+        }
+    }
+    // Si el nodo actual, que es el que quiero borrar, tiene hijos entonces borro su definicion
+    // borro la clave y apunto el iterador al final.
+    if (tieneHijos(actual)) {
+        delete actual->definicion;
+        actual->definicion = NULL;
+        _claves.erase(actual->itClave);
+        actual->itClave = _claves.begin();
+    } else {
+        // Si no, borro desde el ultimo no relevante
+        borradoRecursivo(borrarDesde);
+        // Arreglo el puntero del ultimo relevante si existe, y sino existe es que borre desde la raiz
+        if (ultimoRelevante) {
+            ultimoRelevante->siguientes[indiceArreglar] = NULL;
+        } else {
+            _raiz = NULL;
+        }
+    }
+}
