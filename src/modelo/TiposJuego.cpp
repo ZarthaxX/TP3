9a2290e814354d8a3b768e5af0f0e376fafb192b
Tomas Tropea <tomastropea@hotmail.com>
2019-07-02 22:03:26 -0300

    Update TiposJuego.cpp

--- src/modelo/TiposJuego.cpp
+++ src/modelo/TiposJuego.cpp
@@ -16,15 +16,18 @@ PosYDir::PosYDir() {}
 
 PosYDir::PosYDir(Pos p, Dir d) : pos(p), dir(d) {}
 
+
 ostream &operator<<(ostream &os, const Evento &e) {
+    map<int,string> dir_s = {{ARRIBA,"ARRIBA"},{ABAJO,"ABAJO"},{DERECHA,"DERECHA"},{IZQUIERDA,"IZQUIERDA"}};
   os << "Evento(pos=<" << e.pos.first << ", " << e.pos.second << ">,";
-  os << " dir=" << e.dir << ", " << e.dispara << ")";
+  os << " dir=" << (dir_s.find(e.dir) == dir_s.end() ? "ERROR " : dir_s[e.dir]) << ", " << e.dispara << ")";
   return os;
 }
 
 ostream &operator<<(ostream &os, const PosYDir &pd) {
+    map<int,string> dir_s = {{ARRIBA,"ARRIBA"},{ABAJO,"ABAJO"},{DERECHA,"DERECHA"},{IZQUIERDA,"IZQUIERDA"}};
   os << "(pos=<" << pd.pos.first << ", " << pd.pos.second << ">,";
-  os << " dir=" << pd.dir << ")";
+  os << " dir=" << (dir_s.find(pd.dir) == dir_s.end() ? "ERROR " : dir_s[pd.dir]) << ")";
   return os;
 }
 
